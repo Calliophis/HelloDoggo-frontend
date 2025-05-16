37e6ddcbeadd8f5d1a267299ad2edac681c8d56c
Lisa <lisa.auger2@gmail.com>
2025-05-16 17:29:36 +0200

    feat: add PasswordInputComponent

---
+++ src/app/core/auth/password.rules.ts
@@ -0,0 +1,30 @@
+export type PasswordRule = {
+    key: string;
+    label: string;
+    check: (value: string) => boolean;
+};
+
+export const passwordRules: PasswordRule[] = [
+    {
+        key: 'minLength',
+        label: '8 characters',
+        check: (value) => value.length >= 8
+    },
+    {
+        key: 'upperCase',
+        label: 'One uppercase letter',
+        check: (value) => /[A-Z]+/.test(value)
+    },{
+        key: 'lowerCase',
+        label: 'One lowercase letter',
+        check: (value) => /[a-z]+/.test(value)
+    },{
+        key: 'number',
+        label: 'One number',
+        check: (value) => /[0-9]+/.test(value)
+    },{
+        key: 'specialCharacter',
+        label: 'One special character',
+        check: (value) => /[!@#$%^&*]+/.test(value)
+    },
+]
